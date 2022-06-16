dcdd6fa776c06780477ad7b72fbf47a0e45bcb69
NSTaft <taftnathan13@gmail.com>
2022-06-16 12:02:55 -0500

    Connections updated. Put in commented out databaseURI

--- models/connections.js
+++ models/connections.js
@@ -2,13 +2,33 @@
 // Import Our Dependencies
 /////////////////////////////////////////////
 require("dotenv").config()
+
 const mongoose = require("mongoose")
+
+//////////////MONGODB COPY PASTE/////////////
+// Fire off the connection to Mongo DB
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+
+// mongoose.connection.on('connected', () => {
+//   console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`);
+// });
+
+// mongoose.connection.on("error", (err) => {
+//   console.log("Could not connect to MongoDB!", err);
+// });
+
+
+
 
 /////////////////////////////////////////////
 // Database Connection
 /////////////////////////////////////////////
 // Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_URL = process.env.MONGODB_URI
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true,

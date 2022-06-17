8cfadb14ddf819ba40d0108ea9756ab097dc4505
NSTaft <taftnathan13@gmail.com>
2022-06-16 19:26:44 -0500

    User schema created in models/user.js

---
+++ models/user.js
@@ -0,0 +1,29 @@
+// Import dependencies
+ const mongoose = require('mongoose')
+
+ // Define model
+
+ // Pull Schema and model from mongoose
+
+ const { Schema, modal } = mongoose
+
+ // Make user schema
+
+ const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// Make user model
+
+const User = model('User', userSchema)
+
+// Export user model
+module.exports = User
